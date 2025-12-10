45eda635dd854d785fcd2e6f081068e84ec7bd02
Habibul Islam <habibulislam6862@gmail.com>
2025-12-10 16:40:30 +0600

    footer adjusted

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,4 @@
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
